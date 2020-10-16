6dc93abe9c12bc254e12b6272335a5ec4f70f270
maulana01 <maulanaimammalik4@gmail.com>
2020-10-16 13:55:59 +0700

    new commit

---
+++ src/if1/pkg10119010/latihan9/teks/IF110119010Latihan9Teks.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119010.latihan9.teks;
+
+/**
+ *
+ * @author
+ * NAMA     : Maulana Imam Malik
+ * KELAS    : IF1
+ * NIM      : 10119010
+ * Deskripsi Program : Program ini berisi program untuk menampilkan value
+ *                     dengan tipe data teks
+ */
+public class IF110119010Latihan9Teks {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        char c;
+        char tab = '\t';
+        char newline = '\n';
+        c = 'A';
+        System.out.println("Hasil " + newline + "Char " + tab + "berisi " + c);
+    }
+    
+}
